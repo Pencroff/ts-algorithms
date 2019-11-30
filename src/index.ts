fed634ff67dc632f34075232af9851560f12f226
Sergey Daniloff @Pencroff <pencroff@gmail.com>
2019-11-30 22:56:59 +0000

    Update #7 (exports experiment)

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by Pencroff on 30-Nov-2019.
+ */
+
+import * as primitive from './primitive';
+
+export { primitive };
---
+++ src/primitive/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by Pencroff on 30-Nov-2019.
+ */
+
+import * as comparator from './comparator';
+
+export { comparator };
