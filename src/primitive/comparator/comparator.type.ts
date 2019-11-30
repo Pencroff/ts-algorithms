a85a68f7eed7c3d97bc10da4244938d1280a1cba
Sergey Daniloff @Pencroff <pencroff@gmail.com>
2019-11-30 23:10:32 +0000

    [update] update docs

--- src/primitive/comparator/comparator.type.ts
+++ src/primitive/comparator/comparator.type.ts
@@ -1,12 +1,12 @@
-/**
- * Created by Pencroff on 30-Nov-2019.
- */
 
-/**
- * Comparator function expect two parameters with the same type
- * return {number}
- *    - 0 - parameters equal
- *    - < 0 - v1 less then v2
- *    - > 0 - v1 great then v2
- */
-export type ComparatorFn<T> = (v1: T, v2: T) => number;
+namespace Comparator {
+  /**
+   * Comparator function expect two parameters with the same type
+   * @return {number}
+   *    parameters equal - return zero
+   *    a less then b - return less then zero
+   *    a great then b - return great then zero
+   */
+  export type ComparatorFn<T> = (a: T, b: T) => number;
+}
+
--- src/primitive/comparator/generic.comparator.ts
+++ src/primitive/comparator/generic.comparator.ts
@@ -1,19 +1,22 @@
-/**
- * Generic comparator
- * @param v1
- * @param v2
- * return {number}
- */
-export function genericComparator<T>(v1: T, v2: T): number {
-  let res;
-  if (v1 === v2) {
+
+namespace Comparator {
+  /**
+   * Generic comparator
+   * @param a
+   * @param b
+   * @return {number} - check [[ComparatorFn]]
+   */
+  export function genericComparator<T>(a: T, b: T): number {
+    let res;
+    if (a === b) {
       res = 0;
-  }
-  if (v1 < v2) {
+    }
+    if (a < b) {
       res = -1;
-  }
-  if (v1 > v2) {
+    }
+    if (a > b) {
       res = 1;
+    }
+    return res;
   }
-  return res;
 }
