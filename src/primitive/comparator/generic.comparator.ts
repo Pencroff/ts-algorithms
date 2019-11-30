3b0c78e581112018d8f265d863d05c615c8d072c
Sergey Daniloff @Pencroff <pencroff@gmail.com>
2019-11-30 21:37:34 +0000

    Update #1

--- src/primitive/comparator/generic.comparator.ts
+++ src/primitive/comparator/generic.comparator.ts
@@ -1,7 +1,9 @@
 /**
- * Created by Pencroff on 30-Nov-2019.
+ * Generic comparator
+ * @param v1
+ * @param v2
+ * return {number}
  */
-
 export function genericComparator<T>(v1: T, v2: T): number {
   let res;
   if (v1 === v2) {
