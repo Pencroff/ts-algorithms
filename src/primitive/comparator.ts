de7700c61471e5415361c476e8d6c591665bfccf
Sergey Daniloff @Pencroff <pencroff@gmail.com>
2019-12-01 00:24:44 +0000

    [update] update docs with module

--- src/primitive/comparator.ts
+++ src/primitive/comparator.ts
@@ -1,33 +1,35 @@
+/**
+ * Primitive module contain basic types used across algorithms and data structures
+ * @module primitive
+ */
 
-namespace primitive {
 
-  /**
-   * Comparator function expect two parameters with the same type
-   * @return {number} represent comparison operation, check below
-   *    + parameters equal - return zero
-   *    + a less then b - return less then zero
-   *    + a great then b - return great then zero
-   */
-  export type ComparatorFn<T> = (a: T, b: T) => number;
+/**
+ * Comparator function expect two parameters with the same type
+ * @typeparam T type of both input parameters
+ * @return {number} represent comparison operation, check below
+ *    + parameters equal - return zero
+ *    + a less then b - return less then zero
+ *    + a great then b - return great then zero
+ */
+export type ComparatorFn<T> = (a: T, b: T) => number;
 
-  /**
-   * Generic comparator
-   * @param a
-   * @param b
-   * @return {number} - check [[ComparatorFn]]
-   */
-  export function genericComparator<T>(a: T, b: T): number {
-    let res;
-    if (a === b) {
-      res = 0;
-    }
-    if (a < b) {
-      res = -1;
-    }
-    if (a > b) {
-      res = 1;
-    }
-    return res;
+/**
+ * Generic comparator
+ * @param a
+ * @param b
+ * @return {number} - check [[ComparatorFn]]
+ */
+export function genericComparator<T>(a: T, b: T): number {
+  let res;
+  if (a === b) {
+    res = 0;
   }
-
+  if (a < b) {
+    res = -1;
+  }
+  if (a > b) {
+    res = 1;
+  }
+  return res;
 }
